835da8953f924e07624c8d42892c0ca9e22308e1
Vishvendra Singh Rathore <114246168+vishvendra-singh-rathore@users.noreply.github.com>
2026-02-17 16:23:42 +0530

    Add files via upload

---
+++ imports/Container.tsx
@@ -0,0 +1,3 @@
+export default function Container() {
+  return <div className="opacity-1 size-full" data-name="Container" style={{ backgroundImage: "linear-gradient(rgb(15, 23, 42) 0.17233%, rgba(0, 0, 0, 0) 0.17233%), linear-gradient(90deg, rgb(15, 23, 42) 0%, rgba(0, 0, 0, 0) 0%)" }} />;
+}
